0c819ec996096d295e9d532aad267c32339960cf
Mark DeLillo <mdelillo@pivotal.io>
2017-06-01 16:24:50 -0400

    Create default_rootfs_path with reference to microsoft/windowsservercore image
    [#146101983]
    
    Signed-off-by: Sam Smith <sesmith177@gmail.com>

---
+++ jobs/winc/templates/pre-start.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = "Stop";
+trap { $host.SetShouldExit(1) }
+
+mkdir -f "C:\var\vcap\store\winc"
+
+$rootfsPath = (docker inspect microsoft/windowsservercore | ConvertFrom-Json).GraphDriver.Data.Dir
+
+Set-Content "C:\var\vcap\store\winc\default_rootfs_path" $rootfsPath
