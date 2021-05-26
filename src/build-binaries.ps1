996cbf62eb3cce98ed77c501ec914a8966001d90
Anthony Emengo <aemengo@vmware.com>
2021-05-26 19:11:41 +0000

    Update build-binaries

--- src/build-binaries.ps1
+++ src/build-binaries.ps1
@@ -5,19 +5,23 @@ go version
 
 Write-Host "Build winc"
 
-go build -o "$env:GOBIN\winc.exe" code.cloudfoundry.org/winc/cmd/winc
-if (!(Get-Command "winc.exe" -ErrorAction SilentlyContinue)) {
-   Write-Host "Unable to build winc.exe"
-   Exit 1
-}
+Push-Location src/code.cloudfoundry.org/winc
+   go build -o "$env:GOBIN\winc.exe" ./cmd/winc
+   if (!(Get-Command "winc.exe" -ErrorAction SilentlyContinue)) {
+      Write-Host "Unable to build winc.exe"
+      Exit 1
+   }
+Pop-Location
 
 Write-Host "Build groot"
 
-go build -o "$env:GOBIN\groot.exe" code.cloudfoundry.org/groot-windows
-if (!(Get-Command "groot.exe" -ErrorAction SilentlyContinue)) {
-   Write-Host "Unable to build groot.exe"
-   Exit 1
-}
+Push-Location src/code.cloudfoundry.org/groot-windows
+   go build -o "$env:GOBIN\groot.exe" .
+   if (!(Get-Command "groot.exe" -ErrorAction SilentlyContinue)) {
+      Write-Host "Unable to build groot.exe"
+      Exit 1
+   }
+Pop-Location
 
 Write-Host "Build quota.dll"
 
@@ -40,39 +44,23 @@ if (!(Test-Path $quotaDllPath)) {
    Exit 1
 }
 
-Write-Host "Build winc-network"
+# TODO: renable building winc-network
+# Write-Host "Build winc-network"
 
-go build -o "$env:GOBIN\winc-network.exe" code.cloudfoundry.org/winc/cmd/winc-network
-if (!(Get-Command "winc-network.exe" -ErrorAction SilentlyContinue)) {
-   Write-Host "Unable to build winc-network.exe"
-   Exit 1
-}
-
-Write-Host "Build firewall.dll"
-
-$wincNetworkParent = Split-Path (Get-Command winc-network.exe).Path
-$firewallDllPath = Join-Path "$wincNetworkParent" "firewall.dll"
-if (!(Get-Command "gcc.exe" -ErrorAction SilentlyContinue)) {
-   Write-Host "Unable to find gcc.exe"
-   Exit 1
-}
-gcc.exe -c "$PSScriptRoot\code.cloudfoundry.org\winc\network\firewall\dll\firewall.c" -o firewall.o
-if ($LastExitCode -ne 0) {
-      exit $LastExitCode
-}
-gcc.exe -shared -o "$firewallDllPath" firewall.o -lole32 -loleaut32
-if ($LastExitCode -ne 0) {
-      exit $LastExitCode
-}
-if (!(Test-Path $firewallDllPath)) {
-   Write-Host "Unable to find firewall.dll in the same directory as winc-network.exe"
-   Exit 1
-}
+# Push-Location src/code.cloudfoundry.org/winc
+#   go build -o "$env:GOBIN\winc-network.exe" ./cmd/winc-network
+#   if (!(Get-Command "winc-network.exe" -ErrorAction SilentlyContinue)) {
+#      Write-Host "Unable to build winc-network.exe"
+#      Exit 1
+#   }
+# Pop-Location
 
 Write-Host "Build nstar"
 
-go build -o "$env:GOBIN\nstar.exe" nstar
-if (!(Get-Command "nstar.exe" -ErrorAction SilentlyContinue)) {
-   Write-Host "Unable to build nstar.exe"
-   Exit 1
-}
+Push-Location src/nstar
+  go build -o "$env:GOBIN\nstar.exe" .
+  if (!(Get-Command "nstar.exe" -ErrorAction SilentlyContinue)) {
+     Write-Host "Unable to build nstar.exe"
+     Exit 1
+  }
+Pop-Location
