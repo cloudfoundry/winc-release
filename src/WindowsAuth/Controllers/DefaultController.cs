0164b1a256160c85162c727d10c62fd28b9c5c71
Chris Selzo <cselzo@vmware.com>
2022-05-19 23:12:37 +0000

    Add minimal sample app for windows auth

---
+++ src/WindowsAuth/Controllers/DefaultController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Web.Http;
+
+namespace WindowsAuth.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [Route("~/")]
+        [AllowAnonymous]
+        [HttpGet]
+        public IHttpActionResult Root()
+        {
+            return Ok("Howdy! I am a Windows Authentication test app!");
+        }
+
+        [Route("~/auth")]
+        [HttpGet]
+        [Authorize]
+        public IHttpActionResult Auth()
+        {
+            var identity = Thread.CurrentPrincipal.Identity;
+            return Ok($"Logged in as {identity.Name} via method {identity.AuthenticationType}.");
+        }
+    }
+}
