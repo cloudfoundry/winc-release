9f58eb9fed35e6f04cc024acaa848293848efc04
David Sabeti <sabetid@vmware.com>
2022-05-25 23:54:28 +0000

    Use local groot.exe for tests if present
    Signed-off-by: Chris Selzo <cselzo@vmware.com>

--- scripts/test-using-fly.sh
+++ scripts/test-using-fly.sh
@@ -5,15 +5,19 @@ export GARDEN_WINDOWS_TARGET="garden-windows"
 
 groot_path="$HOME/workspace/winc-release/src/code.cloudfoundry.org/groot-windows"
 build_groot_task="$HOME/workspace/garden-windows-ci/tasks/build-groot/task.yml"
-groot_output=$(mktemp -d)
 
-echo "Building groot via ${build_groot_task}..."
-fly --target "$GARDEN_WINDOWS_TARGET" e -p \
-  --tag 2019 \
-  --inputs-from "winc/winc-2019" \
-  -i groot-windows="${groot_path}" \
-  -c "${build_groot_task}" \
-  -o groot-binary="${groot_output}"
+if [[ -f "${groot_path}/groot.exe" ]]; then
+    echo "groot binary already exists at ${groot_path}, using that one."
+else
+  echo "Building groot via ${build_groot_task}..."
+  fly --target "$GARDEN_WINDOWS_TARGET" e -p \
+    --tag 2019 \
+    --inputs-from "winc/winc-2019" \
+    -i groot-windows="${groot_path}" \
+    -c "${build_groot_task}" \
+    -o groot-binary="${groot_path}"
+fi
+
 
 winc_path="$HOME/workspace/winc-release/src/code.cloudfoundry.org/winc"
 build_winc_task="$HOME/workspace/garden-windows-ci/tasks/test-winc/task.yml"
@@ -25,8 +29,9 @@ export WINDOWS_VERSION=2019
 echo "Running task ${build_winc_task}"
 fly --target "$GARDEN_WINDOWS_TARGET" e -p \
   --tag 2019 \
-  --inputs-from "winc/winc-2019" \
+  --include-ignored \
   -i winc="${winc_path}" \
-  -i groot-binary="${groot_output}" \
+  -i ci="${HOME}/workspace/garden-windows-ci" \
+  -i groot-binary="${groot_path}" \
   -c "${build_winc_task}" \
   "$@"
